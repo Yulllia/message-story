1c390d249a1a23eba30a7752a60c98f5ae850904
Yulia <shyshka20@gmail.com>
2022-08-20 22:08:31 +0300

    Update SignInImage.jsx

--- client/src/pages/signImage/SignInImage.jsx
+++ client/src/pages/signImage/SignInImage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import noAvatar from "../../assets/jpg/no-avatar.png";
 import { BiLogOut } from "react-icons/bi";
-import { useState, useContext } from "react";
+import { useState, useContext, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
@@ -10,37 +10,66 @@ function SignInImage() {
   const [showLogout, setShowLogout] = useState(false);
   const navigate = useNavigate();
   const auth = useContext(AuthContext);
-  
-  function logoutHandler(e) {
-    e.preventDefault();
-    setShowLogout(!showLogout);
+  const logoutRef = useRef(null);
+
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (logoutRef.current && !logoutRef.current.contains(event.target)) {
+          setShowLogout(false)
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [logoutRef]);
+
+
+  function logoutHandler() {
     auth.logout();
     navigate("/sign-in");
   }
-  
- function logOutField() {
+
+  function logOutField() {
     return (
-      <span className="logoutBlock">
+      <span className="logoutBlock" onClick={logoutHandler} ref={logoutRef}>
         <p className="logoutTitle">Log out</p>
         <BiLogOut className="logoutImage" />
       </span>
     );
   }
-  
+  console.log(showLogout);
+
   return (
     <div className="imageSignIn">
       {data?.image?.length > 0 ? (
         <>
-          <img className={`userImage ${showLogout ? "withLogout" : "noLogout"}`} src={`${data?.image}`} alt="userpicture" onClick={logoutHandler}/>
-          <div className="colored-circle" style={{ bottom: showLogout ? "54px" : "24px" }}>
+          <img
+            className={`userImage ${showLogout ? "withLogout" : "noLogout"}`}
+            onClick={() => setShowLogout(!showLogout)}
+            src={`${data?.image}`}
+            alt="userpicture"
+          />
+          <div
+            className="colored-circle"
+            style={{ bottom: showLogout ? "54px" : "24px" }}
+          >
             <div className="checkmark"></div>
           </div>
           {showLogout && logOutField()}
         </>
       ) : (
         <>
-          <img className={`userImage ${showLogout ? "withLogout" : "noLogout"}`} src={`${data?.image}`} alt="userpicture" onClick={logoutHandler}/>
-          <div className="colored-circle" style={{ bottom: showLogout ? "54px" : "24px" }}>
+          <img
+            className={`userImage ${showLogout ? "withLogout" : "noLogout"}`}
+            src={`${noAvatar}`}
+            onClick={() => setShowLogout(!showLogout)}
+            alt="userpicture"
+          />
+          <div
+            className="colored-circle"
+            style={{ bottom: showLogout ? "54px" : "24px" }}
+          >
             <div className="checkmark"></div>
           </div>
           {showLogout && logOutField()}
@@ -51,3 +80,4 @@ function SignInImage() {
 }
 
 export default SignInImage;
+
